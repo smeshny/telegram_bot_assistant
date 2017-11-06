599ec88ff0ec4d06de8e134595c5d466b1a8a0b2
smeshny <smeshny@gmail.com>
2017-11-06 17:40:02 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,114 @@
+import config
+import telebot
+from telebot import types
+import time
+import cmc_api
+import random
+import TextFiles
+from multiprocessing import Process
+
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['menu'])
+def get_market1(message):
+    markup = types.ReplyKeyboardMarkup(True, False)
+    itembtn1 = types.KeyboardButton('/market')
+    itembtn2 = types.KeyboardButton('/ETH')
+    itembtn3 = types.KeyboardButton('/BTC')
+    itembtn4 = types.KeyboardButton('/LTC')
+    itembtn5 = types.KeyboardButton('/ETC')
+    itembtn6 = types.KeyboardButton('/DCR')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6)
+    bot.send_message(message.chat.id, "Push the button", reply_markup=markup)
+
+
+@bot.message_handler(commands=['cap'])
+def go_coin_market_cap(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на coinmarketcap.com", url="https://coinmarketcap.com/")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Push the button", reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['vital'])
+def get_market(message):
+    bot.send_message(message.chat.id, '@spielbergos - Спилбергос стивенушечка!')
+    quote = random.choice(TextFiles.quotes)
+    bot.send_message(message.chat.id, '``` ' + quote + ' ```', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['coinflip'])
+def get_market(message):
+    sequence = ['Орёл', 'Решка']
+    r = random.choice(sequence)
+    bot.send_message(message.chat.id, r)
+
+
+@bot.message_handler(commands=['market'])
+def get_market(message):
+    if message.text[0] == '/':
+        time.sleep(1)
+        bot.delete_message(message.chat.id, message.message_id)
+    temp = bot.send_message(message.chat.id, cmc_api.get_market())
+    time.sleep(15)
+    bot.delete_message(message.chat.id, temp.message_id)
+
+
+def bitcoin_checker():
+    while True:
+        try:
+            price = int((float((cmc_api.get_markets('BTC'))[20:28])) / 100)
+            time.sleep(305)
+            new_price = int((float((cmc_api.get_markets('BTC'))[20:28])) / 100)
+            if new_price > price:
+                what = 'Биток пробил ' + str(new_price * 100) + '. Цена: ' + str((cmc_api.get_markets('BTC'))[20:28])
+                bot.send_message(chat_id=-1001081308494, text='``` ' + what + ' ```', parse_mode='Markdown')
+            elif new_price < price:
+                what = 'Биток упал ниже ' + str(price * 100) + '. Цена: ' + str((cmc_api.get_markets('BTC'))[20:28])
+                bot.send_message(chat_id=-1001081308494, text='``` ' + what + ' ```', parse_mode='Markdown')
+            else:
+                continue
+        except:
+            time.sleep(305)
+            continue
+
+
+@bot.message_handler(func=lambda message: True, content_types=['sticker'])
+def sticker_deleter(message):
+    sticker = message.message_id
+    time.sleep(22)
+    bot.delete_message(message.chat.id, sticker)
+
+
+@bot.message_handler(func=lambda m: True)
+def get_price(message):
+    if cmc_api.get_markets(message.text[1:]) == 'Такой команды или валюты нету':
+        pass
+    else:
+        temp = bot.send_message(message.chat.id, cmc_api.get_markets(message.text[1:]))
+        time.sleep(15)
+        bot.delete_message(message.chat.id, temp.message_id)
+    if message.text[0] == '/':
+        time.sleep(1)
+        bot.delete_message(message.chat.id, message.message_id)
+
+
+if __name__ == '__main__':
+    m1 = Process(target=bitcoin_checker)
+    # m2 = Process(target=bot_polling)
+    m1.start()
+    # m2.start()
+    # m1.join()
+    # m2.join()
+    bot.polling(none_stop=True)
+
+
+'''
+menu - вызвать меню
+vital - вызвать виталика
+cap - перейти на coinmarketcap.com
+coinflip - бросить монетку
+market - узнать капитализацию
+'''
---
+++ cmc_api.py
@@ -0,0 +1,36 @@
+from coinmarketcap import Market
+
+
+def get_markets(exchange):
+    try:
+        coinmarketcap = Market()
+    except:
+        return 'Неполадки на сервере coinmarketcap'
+    get_m = coinmarketcap.ticker(limit=900, convert='USD')
+    for i in get_m:
+        if i['symbol'] == exchange.upper():
+            return i['name'] + ':' + '\n' + \
+                   'USD price: ' + i['price_usd'] + '\n' + \
+                   'BTC price: ' + i['price_btc'] + '\n' + \
+                   'Change 1h: ' + i['percent_change_1h'] + '%' + '\n' + \
+                   'Change 24h: ' + i['percent_change_24h'] + '%' + '\n' + \
+                   'Change 7d: ' + i['percent_change_7d'] + '%' + '\n' + \
+                   '24H Volume: ' + str(round(float(i['24h_volume_usd']) / 1000000000, 3)) + ' B' + '\n' + \
+                   'Market cap USD: ' + str(round(float(i['market_cap_usd']) / 1000000000, 3)) + ' B' + '\n'
+        else:
+            continue
+    return 'Такой команды или валюты нету'
+
+
+def get_market():
+    try:
+        coinmarketcap = Market()
+    except:
+        return 'Неполадки на сервере coinmarketcap'
+    request = coinmarketcap.stats()
+    return 'Market Cap: ' + str(round(request['total_market_cap_usd'] / 1000000000, 3)) + ' B' + '\n' + \
+           '24h Vol: ' + str(round(request['total_24h_volume_usd'] / 1000000000, 3)) + ' B' + '\n' + \
+           'BTC Dominance: ' + str(round(request['bitcoin_percentage_of_market_cap'], 2)) + '%' + '\n' + \
+           'Active currencies: ' + str(int(request['active_currencies']))
+
+
